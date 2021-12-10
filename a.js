d5ad9398df5e9ea1fb2d7e25afb7de0c7ee10d2c
zanaviska <31745527+zanaviska@users.noreply.github.com>
2021-12-10 16:57:42 +0200

    Update a.js

--- a.js
+++ a.js
@@ -3,23 +3,25 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const alfa_token = '5081789260:AAGTTBOUi4xPsqrsFWpVvdJSGURcFbsmOE4';
-const beta_token = '5029291032:AAFAfquSVFnEjDKTWGsLWxXPJG2KkkJ3QQY';
+const alfa_token = '5090989926:AAFe4gR5yfdihI7vfqZIekNG0vsfo_eKd4E';
+const beta_token = '5062338123:AAFWiJQxLbu-f3Lej1cNfAqAhSQL4XfcvWI';
 
-const day_f_chat_id = -1001651566770;
+// const day_f_chat_id = -1001651566770;//for tests
+const day_f_chat_id = -1001725276250;//real one
 const tokens_chat_id = -1001733712953;
 
-const msgA = "Це повідомлення А";
 
-const alfa_login = "спец код альфи";
-const alfa_msgB = "повідомлення Б від альфи";
-const alfa_password = "пароль альфи";
-const alfa_msgC = "повідомлення В альфи";
+const alfa_msgA = "Greetings, how can I help you?";
+const alfa_login = "rabbitHole".toLocaleLowerCase();
+const alfa_msgB = "Specify your problem";
+const alfa_password = "lostInTheF".toLocaleLowerCase();
+const alfa_msgC = "Out of my jurisdiction. Please contact another agent";
 
-const beta_login = "спец код бети";
-const beta_msgB = "повідомлення Б від бети";
-const beta_password = "пароль бети";
-const beta_msgC = "повідомлення В бети";
+const beta_msgA = "Beschreiben Sie Ihr Problem";
+const beta_login = "Wachter".toLocaleLowerCase();
+const beta_msgB = "Klären Sie Ihr Problem";
+const beta_password = "Täuschung".toLocaleLowerCase();
+const beta_msgC = "Anfrage bearbeiten, github.com/artoman42/Agent-1984";
 
 // Create a bot that uses 'polling' to fetch new updates
 const alfa_bot = new TelegramBot(alfa_token, {polling: true});
@@ -28,14 +30,24 @@ const beta_bot = new TelegramBot(beta_token, {polling: true});
 const userMsgRemoveTime = 3000;
 const repeatMsgTime = 45000;
 
+const repeatAction = func => {
+  func().catch(async res => {
+    if (e.response.statusCode == 429) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      repeatAction(func);
+    } else {
+      console.log(e.response)
+    } 
+  })
+}
+
 function recursiveSender(bot, message, callback) {
-  const msg = bot.sendMessage(day_f_chat_id, message);
-  msg.then(res => {
+  repeatAction(() => bot.sendMessage(day_f_chat_id, message).then(res => {
     setTimeout(() => {
-      bot.deleteMessage(day_f_chat_id, res.message_id);
+      repeatAction(() => bot.deleteMessage(day_f_chat_id, res.message_id));
       setTimeout(() => callback(bot, message, callback), repeatMsgTime)
     }, repeatMsgTime);
-  })
+  }))
 }
 
 const beforeLoginState = 0;
@@ -45,11 +57,10 @@ const correctPasswordState = 3;
 
 const temproraryUnlistenedUsers = [];
 
-function onMessage(bot, msgB, loginSpecCode, password, msgC, callback, othercallback) {
+function onMessage(bot, msgA, msgB, loginSpecCode, password, msgC, callback, othercallback) {
   const userState = [];
   return () => {
     bot.on('message', (msg) => {
-      
       if (msg.chat.type !== 'private' && msg.chat.id !== day_f_chat_id)
         return;
 
@@ -60,18 +71,19 @@ function onMessage(bot, msgB, loginSpecCode, password, msgC, callback, othercall
       callback(chatId, msgId)
       
       if (chatId == day_f_chat_id)
-      return;
+        return;
       
+      // console.log(msgId)
+      
+      if (temproraryUnlistenedUsers[senderId])
+        return;
       switch (userState[senderId]) {
         case afterLoginState:
-          if (temproraryUnlistenedUsers[senderId])
-            break;
           
-          if (msg.text == password) {
+          if (msg.text.toLocaleLowerCase() == password) {
             userState[senderId] = correctPasswordState;
-            bot.sendMessage(tokens_chat_id, `@${msg.from.username} #password`).catch(() => { })
-            othercallback(msg.from);
-            bot.sendMessage(chatId, msgC);
+            repeatAction(() => bot.sendMessage(tokens_chat_id, `@${msg.from.username} #password`).catch(() => { }))
+            repeatAction(() => bot.sendMessage(chatId, msgC).then(res => {othercallback(msg.from, res.message_id);}));
           }
           else {
             temproraryUnlistenedUsers[senderId] = true;
@@ -86,12 +98,16 @@ function onMessage(bot, msgB, loginSpecCode, password, msgC, callback, othercall
           break;
         //for beforeLoginState and new user aka undefined
         default:
-          if (msg.text == loginSpecCode) {
+          if (msg.text.toLocaleLowerCase() == loginSpecCode) {
             userState[senderId] = afterLoginState;
-            bot.sendMessage(chatId, msgB);
-            bot.sendMessage(tokens_chat_id, `@${msg.from.username} #login`).catch(() => { })
+            repeatAction(() => bot.sendMessage(chatId, msgB));
+            repeatAction(() => bot.sendMessage(tokens_chat_id, `@${msg.from.username} #login`).catch(e => {}))
           } else { 
             bot.sendMessage(chatId, msgA);
+            temproraryUnlistenedUsers[senderId] = true;
+            setTimeout(() => {
+              temproraryUnlistenedUsers[senderId] = false;
+            }, 5000);
           }
           break;
       }
@@ -99,26 +115,30 @@ function onMessage(bot, msgB, loginSpecCode, password, msgC, callback, othercall
   }
 }
 
-recursiveSender(alfa_bot, "this is alpha bot", recursiveSender)
-setTimeout(() => recursiveSender(beta_bot, "this is beta bot", recursiveSender), repeatMsgTime);
+recursiveSender(alfa_bot, "If you have a complaint against the administration, describe your complaint to the agents", recursiveSender)
+setTimeout(() => recursiveSender(beta_bot, "Die Agenten halten Ordnung im Chat, wenn dir etwas auffällt, lass es uns bitte wissen", recursiveSender), repeatMsgTime);
 // regular_sender_clojure(beta_bot)("this is beta bot")
 // regular_sender_clojure(alfa_bot)("this is alpha bot")
 
 const waitForToken = [];
 
-onMessage(alfa_bot, alfa_msgB, alfa_login, alfa_password, alfa_msgC, function (chatId, msgId) {
+onMessage(alfa_bot, alfa_msgA, alfa_msgB, alfa_login, alfa_password, alfa_msgC, function (chatId, msgId) {
   if (chatId !== day_f_chat_id)
     return;
   // console.log(msgId)
   setTimeout(() => {
-    alfa_bot.deleteMessage(chatId, msgId);
+    repeatAction(() => alfa_bot.deleteMessage(chatId, msgId));
   }, userMsgRemoveTime)
-}, () => { })()
-onMessage(beta_bot, beta_msgB, beta_login, beta_password, beta_msgC, (chatId, msgId) => { }, (user) => {
+}, (user, ) => {
+  beta_bot.restrictChatMember(day_f_chat_id, user.id).catch((err) => {
+    console.log("trying to remove admin");
+  })
+})()
+onMessage(beta_bot, beta_msgA, beta_msgB, beta_login, beta_password, beta_msgC, (chatId, msgId) => { }, (user) => {
   waitForToken[user.id] = user;
   beta_bot.banChatMember(day_f_chat_id, user.id).then(res => {
     // console.log(res)
-    beta_bot.unbanChatMember(day_f_chat_id, user.id);
+    // beta_bot.unbanChatMember(day_f_chat_id, user.id);
   }).catch((err) => {
     console.log("trying to remove admin");
   })
@@ -128,9 +148,12 @@ beta_bot.onText(/\/sendcontact (.+)/, (msg, match) => {
   const userId = msg.from.id;
   const resp = match[1]; // the captured "whatever"
 
+  
   const user = waitForToken[userId];
   if (user) {
-    beta_bot.sendMessage(tokens_chat_id, `@${user.username} #token ${resp}`);
+    repeatAction(() => beta_bot.sendMessage(tokens_chat_id, `@${user.username} #token ${resp}`));
+    repeatAction(() => beta_bot.sendMessage(msg.chat.id, `Your token is ${resp}\nFarewell`));
+    // beta_bot.sendMessage(, `@${user.username} #token ${resp}`);
     waitForToken[userId] = NaN;
   }
 });
